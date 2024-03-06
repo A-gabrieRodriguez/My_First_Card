b47f672e0b90edec54081b95ac4e8b86ff946dc5
Gabriel Rodriguez <argabrielrodriguez15@gmail.com>
2024-03-05 21:48:28 -0600

    :wrench: Se imprime imagen con frutas de la lista

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,10 +2,13 @@ import { Component } from "react";
 
 class Product extends Component {
     render (){
+        const {product} = this.props
         return(
-            <p>
-                fruits_number
-            </p>
+            <div>
+            <img alt={product.name} src={product.img}/>
+            <h3>{product.name}</h3>
+            <p>{product.price }</p>
+            </div>
         )
     }
 }
