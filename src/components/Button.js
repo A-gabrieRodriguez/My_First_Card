15bb1d25dfc4e11164c2fa498e67657939eea06b
Gabriel Rodriguez <argabrielrodriguez15@gmail.com>
2024-03-06 10:40:30 -0600

    :art: Se mejoro el diseño de los cards de los productos y se agrego el buton con su propiedad onClick que imprime mensaje en consola

---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import { Component } from "react";
+
+const styles = {
+    button:{
+    backgroundColor: "#0A283E",
+    color: "#fff",
+    padding:"10px 15px",
+    border:"none",
+    borderRadius: "5px",
+    cursor:"pointer"
+    }
+}
+
+class Button extends Component {
+    render(){
+        return(
+            
+            //La propiedad  ...this.props pasamos la propiedades que 
+            //resive el componente de botton, en este caso el componente
+            //de Button en el archivo product tiene la propiedad de onClick
+            <button style={styles.button} {...this.props}  />
+        )
+    }
+}
+
+export default Button
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,30 @@
 import { Component } from "react";
-
+import Button from "./Button"
+const style = {
+    product:{
+        border: 'solid 1px #eee',
+        boxShadow: '0 5px 5px rgb(0, 0, 0, 0.1)',
+        width: '30%',
+        padding:'10 px 15px',
+        borderRadius:'5px ',
+    },
+    img:{
+        width:'100%',
+        height:"50%"
+    }
+}
 class Product extends Component {
     render (){
-        const {product} = this.props
+        const {product,addProduct} = this.props
+
         return(
-            <div>
-            <img alt={product.name} src={product.img}/>
+            <div style={style.product}>
+            <img style={style.img} alt={product.name} src={product.img}/>
             <h3>{product.name}</h3>
             <p>{product.price }</p>
+            <Button onClick={()=> addProduct(product)}>
+                Add to cart
+            </Button>  
             </div>
         )
     }
